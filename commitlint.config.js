96a4c9b1c3764fc22b971a1b02b79c17e0e6fe09
skezo <793287+skezo@users.noreply.github.com>
2026-01-14 22:21:43 +0800

    chore(ci): add core and server scopes to commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['desktop', 'extension', 'protocol', 'ui', 'docs', 'ci', 'deps']],
+    'scope-enum': [
+      2,
+      'always',
+      ['desktop', 'extension', 'protocol', 'ui', 'core', 'server', 'docs', 'ci', 'deps'],
+    ],
   },
 };
